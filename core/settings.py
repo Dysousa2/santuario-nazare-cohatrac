d396195cac34cbaf3adb6312a1a88d2c93d6e183
Dyemerson <dyemersonsousa@gmail.com>
2025-10-24 08:33:23 -0300

    set secret key

--- core/settings.py
+++ core/settings.py
@@ -16,8 +16,7 @@ from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET_KEY')
-
+SECRET_KEY = config("SECRET_KEY", default="build-secret-key")
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
